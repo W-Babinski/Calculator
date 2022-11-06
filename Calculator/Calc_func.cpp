61fb0cda300f8e612ded1631dfb2c2d822d3a5ec
Wiktor Babiński <wbabinski01@gmail.com>
2022-11-07 00:47:48 +0100

    Added support for pow()

--- Calculator/Calc_func.cpp
+++ Calculator/Calc_func.cpp
@@ -1,4 +1,5 @@
 #include "Calc_func.h"
+#include <cmath>
 
 double Calc_func::Calculate(double x, char oper, double y)
 {
@@ -15,6 +16,9 @@ double Calc_func::Calculate(double x, char oper, double y)
 		break;
 	case '/':
 		return x / y;
+		break;
+	case '^':
+		return pow(x, y);
 		break;
 	default:
 		return 0.0;
